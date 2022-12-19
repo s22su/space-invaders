b0d919235c91fc735b57e05a6e10647e092484f8
Sergei Beregov <sergei.beregov@gmail.com>
2022-12-20 01:55:02 +0200

    Add sounds + play startup sound

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use rusty_audio::Audio;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+
+    audio.add("explode", "./sounds/explode.wav");
+    audio.add("lose", "./sounds/lose.wav");
+    audio.add("move", "./sounds/move.wav");
+    audio.add("pew", "./sounds/pew.wav");
+    audio.add("startup", "./sounds/startup.wav");
+    audio.add("win", "./sounds/win.wav");
+
+    audio.play("startup");
+    audio.wait();
+
+    Ok(())
 }
